d181fd07e379964259756b86163e060442236402
Likitha <lickys44@gmail.com>
2021-06-25 15:50:35 +0530

    changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,25 +4,14 @@ const Contact = () => {
     return (
         <div>
           <h1>Contact Us</h1>
-      <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
+      <form name="contact-v1" 
+      method="POST"
+    data-netlify="true">
+        <input type="hidden" name="form-name" value="contact-v1" />
+     <input type="text" placeholder="First Name" name="name" />
+     <input type="email" placeholder="Email" name="email"/>
+     <textarea placeholder="Any Comment" name="message"/>
+     <button type="submit">Submit</button>
 </form>  
         </div>
     )
