0df25b31074913ebbbb0a9b60dbcb8b318280535
Likitha <lickys44@gmail.com>
2021-06-25 14:54:44 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Contact Us</h1>
+      <form name="contact Web"
+       method="post"
+       data-netlify="true" 
+       onSubmit="submit">
+         <input type="hidden" name="form-name" value="contact Web"/>
+         <div>
+           <label>
+             First Name:<br></br>
+             <input type="text" name="first-name"/>
+           </label>
+         </div>
+         <div>
+           <label htmlFor="email">Email</label><br/>
+           <input type="email" name="email"/>
+         </div>
+         <div>
+           <label htmlFor="comment">Comment</label><br/>
+           <textarea name="comment"></textarea>
+         </div>
+         <button type="submit">Submit</button>
+       </form>
+ 
     </div>
   );
 }
